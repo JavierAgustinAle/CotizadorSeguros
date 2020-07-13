6a229b70f45d685f4e2cbfa202f32b762264ff98
JavierAle <javierale25@live.com>
2020-07-12 21:28:28 -0300

    Updates

---
+++ precache-manifest.15f40dece2667fbd9a1a81bcae2345f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b421fb6ed81f5f2cdc2f8125b2d98c4d",
+    "url": "/CotizadorSeguros/index.html"
+  },
+  {
+    "revision": "25d3e6554570c663392a",
+    "url": "/CotizadorSeguros/static/css/main.2fd0f9b3.chunk.css"
+  },
+  {
+    "revision": "1b0d86ffc546a9b2e89b",
+    "url": "/CotizadorSeguros/static/js/2.09f47de7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CotizadorSeguros/static/js/2.09f47de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d3e6554570c663392a",
+    "url": "/CotizadorSeguros/static/js/main.9ee7894e.chunk.js"
+  },
+  {
+    "revision": "11dbf435ae7ef9c46fa6",
+    "url": "/CotizadorSeguros/static/js/runtime-main.99d9f06d.js"
+  }
+]);
